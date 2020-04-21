ec9884b996e867e3f26fbf218bba83fdc56ca98d
r0ma <mymail@nxt.ru>
2020-04-21 06:55:28 +0500

    2-й коммит

--- background.js
+++ background.js
@@ -1,10 +1,23 @@
 // background.js 
 // Вызывается, когда пользователь нажимает на действие браузера.
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-    // Отправить сообщение на активную вкладку
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      var activeTab = tabs[0];
-      chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//     // Отправить сообщение на активную вкладку
+//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//       var activeTab = tabs[0];
+//       chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+//     });
+// });
+
+
+chrome.tabs.create({
+    active: true,
+    url: 'http://ppif.ru/imei/index.php'
+}, function(tab) {
+    chrome.tabs.executeScript(tab.id, {
+        code: "localStorage.setItem('9999999999', 'zzzzzzzzzzz')"
+        // code: "$.post('http://ppif.ru/imei/parser.php', {text: 'many_many_xxx'});"
+    }, function() {
+        // chrome.tabs.remove(tab.id);
     });
 });
--- content.js
+++ content.js
@@ -1,17 +1,36 @@
 // Скрипт, который работает на всех загружаемых страницах,
 // либо на страницах указанных в файле manifest.json
+// let kod = $('#general_imei_info').text();
+let url = window.location.href;
+
+// console.log("IMEI INFO PARSER: ");
+// console.log(kod);
+
+// function randomInteger(min, max) {
+    // случайное число от min до (max+1)
+    // let rand = min + Math.random() * (max + 1 - min);
+    // return Math.floor(rand);
+// }
+
+// let key = randomInteger(1, 999);
+// localStorage.setItem(key, kod);
+
+// chrome.storage.local.set({url: key}, function() {});
 
 
-console.log("IMEI INFO PARSER");
+
+
+// $.post('http://ppif.ru/imei/parser.php', kod);
+
 // var firstHref = $("a[href^='http']").eq(0).attr("href");
 // console.log(firstHref);
 
-chrome.runtime.onMessage.addListener(
-    function(request) {
-      if( request.message === "clicked_browser_action" ) {
-        var firstHref = $("a[href^='http']").eq(0).attr("href");
+// chrome.runtime.onMessage.addListener(
+//     function(request) {
+//       if( request.message === "clicked_browser_action" ) {
+//         var firstHref = $("a[href^='http']").eq(0).attr("href");
   
-        console.log(firstHref);
-      }
-    }
-);
+//         console.log(firstHref);
+//       }
+//     }
+// );
